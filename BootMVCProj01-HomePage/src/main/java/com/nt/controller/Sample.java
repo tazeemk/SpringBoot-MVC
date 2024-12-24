bba012d26e625e4ce0e192b322ef46a303524655
Tazeem Khan <tazeemk329@gmail.com>
2024-12-25 01:16:14 +0530

    mvc simple home page display

---
+++ BootMVCProj01-HomePage/src/main/java/com/nt/controller/Sample.java
@@ -0,0 +1,18 @@
+package com.nt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Sample 
+{
+	
+@PostMapping("/")
+public String getInsatnce() {
+		return "Welcome";
+
+    }
+	
+}
